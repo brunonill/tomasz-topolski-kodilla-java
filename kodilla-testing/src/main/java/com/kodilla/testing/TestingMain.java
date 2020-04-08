5861d76e5ca6f92f7d38d626ced275613f81f5e5
brunonill <tomasz.topolski@hotmail.comgit config --global user.email tomasz.topolski@hotmail.comgit config --global user.email tomasz.topolski@hotmail.com>
2020-04-08 11:00:50 +0200

    Add Calculator testing

--- kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
@@ -1,9 +1,30 @@
-
-
 package com.kodilla.testing;
+
+import com.kodilla.testing.calculator.Calculator;
+import com.kodilla.testing.user.SimpleUser;
 
 public class TestingMain {
     public static void main(String[] args){
-        System.out.println("Moduł 6. Wprowadzenie to testowania oprogramowania");
+        SimpleUser simpleUser = new SimpleUser("theForumUser");
+
+        String result = simpleUser.getUsername();
+
+        if (result.equals("theForumUser")){
+            System.out.println("test OK");
+        } else {
+            System.out.println("Error!");
+        }
+
+        System.out.println("Calculator test");
+
+        Calculator calculator = new Calculator();
+        Integer addTest = calculator.add(5,5);
+        Integer subTest = calculator.substract(10,5);
+
+        if (addTest.equals(10) && subTest.equals(5)){
+            System.out.println("Test Passed");
+        }else{
+            System.out.println("Error");
+        }
     }
 }
